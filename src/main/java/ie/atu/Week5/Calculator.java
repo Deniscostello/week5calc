461bfab26a190c7a567920cbaa7ab1b7068c0360
Denis Costello <G00396309@atu.ie>
2022-10-19 13:31:51 +0100

    Calculator feature Add test

--- src/main/java/ie/atu/Week5/Calculator.java
+++ src/main/java/ie/atu/Week5/Calculator.java
@@ -1,4 +1,11 @@
 package ie.atu.Week5;
 
 public class Calculator {
+
+
+
+    public int add()
+    {
+       return 1 + 5;
+    }
 }
--- src/test/java/ie/atu/Week5/CalculatorTest.java
+++ src/test/java/ie/atu/Week5/CalculatorTest.java
@@ -2,13 +2,21 @@ package ie.atu.Week5;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
+    Calculator calcValue;
 
     @BeforeEach
     void setUp() {
+        calcValue = new Calculator();
+    }
+
+    @Test
+    void testAdd(){
+        assertEquals(6, calcValue.add());
     }
 
     @AfterEach
